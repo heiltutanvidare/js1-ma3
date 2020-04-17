d418ffe8d605979f36354222855069a54b911a78
Vidar H <vusvol@icloud.com>
2020-04-17 10:31:35 +0200

    Fixed typos

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 //     return a - b;
 // }
 
-const doSomething = (a, b) => a - b;
+const subtract = (a, b) => a - b;
 
 // Question 2
 // Refer: lesson 2 / 4
@@ -64,7 +64,6 @@ if (params.has("userId")) {
 
 // Question 5
 // Write code that removes the button, and only the button, from its parent element in the HTML below:
-
 // <div class="container">
 //     <p>Lorem ipsum dolor</p>
 //     <button class="btn">Click me</button>
@@ -76,7 +75,6 @@ const button = document.querySelector(".btn");
 container.removeChild(button);
 
 // Question 6
-// Refer: lesson 4
 // Create an li element with a text value of Parrots and a class of parrots.
 // Add the new item as the second item in the ul below (add it after Cows).
 // <ul class="animals">
@@ -94,14 +92,10 @@ parrots.className = "parrots";
 cows.after(parrots);
 
 // Question 7
-// Refer: lesson 4
 // Make a call to the URL below and pass the JSON it returns to a function.
-
 // Inside that function select the div from the HTML below and assign
 // the rating property from the JSON object as it's text value.
-
 // In the catch method, log the error that may be returned.
-
 // https://api.rawg.io/api/games/3801
 // <div class="rating"></div>
 
